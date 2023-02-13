a1bd217bb03d9ff157d6d97a27f1495ff513ecf0
Rohit96630 <rohitzare966@gmail.com>
2023-02-13 12:11:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserContext } from "./Components/ContextApi";
+import SideBar from "./Components/SideBar";
+import UserList from "./Components/UserList";
 
 function App() {
+  const [usersData, setUsersData] = useState([]);
+
+  const getUserList = async () => {
+    try {
+      const response = await axios.get("https://panorbit.in/api/users.json");
+      const result = await response.data.users;
+      setUsersData(result);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    getUserList();
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={usersData}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<UserList />} />
+          <Route exact path="/profile/:id" element={<SideBar />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
